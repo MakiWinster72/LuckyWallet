633188ee990982ab6a3a69d7100a80cba3ce5173
Landen <1935149790@qq.com>
2026-07-27 19:37:00 +0800

    test(api): cover bill contract adapters

--- frontend/src/api/bills.js
+++ frontend/src/api/bills.js
@@ -1,38 +1,5 @@
 import { authenticatedApiRequest } from "./auth";
-
-const categoryIds = {
-  餐饮: 1,
-  零食: 2,
-  日用品: 3,
-  聚会: 4,
-  交通: 5,
-  其他: 6,
-};
-
-export function adaptBillFromApi(bill) {
-  return {
-    id: bill.id,
-    title: bill.title,
-    amount: Number(bill.amount),
-    category: bill.category.name,
-    payer: bill.payer_id,
-    participants: bill.participants.map((participant) => participant.user_id),
-    date: bill.bill_date,
-    note: bill.note ?? "",
-  };
-}
-
-export function adaptBillToApi(bill) {
-  return {
-    title: bill.title,
-    amount: Number(bill.amount),
-    payer_id: Number(bill.payer),
-    bill_date: bill.date,
-    category_id: categoryIds[bill.category],
-    participant_ids: bill.participants,
-    note: bill.note || null,
-  };
-}
+import { adaptBillFromApi, adaptBillToApi } from "./bill-adapter";
 
 export async function listBillsApi() {
   const response = await authenticatedApiRequest("/bills");
