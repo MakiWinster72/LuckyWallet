d35bf42b5ae39728973b51c8bc91577c4fd38740
MakiWinster <makiiiiiiiho@gmail.com>
2026-07-29 02:23:07 +0800

    style: remove homepage brand icon shadow

--- frontend/src/components/BrandIcon.jsx
+++ frontend/src/components/BrandIcon.jsx
@@ -10,7 +10,6 @@ export function BrandIcon({ className = "", size = 38 }) {
       style={{
         display: "block",
         borderRadius: "11px 11px 11px 3px",
-        boxShadow: "0 2px 8px rgb(0 0 0 / 15%)",
         objectFit: "cover",
       }}
     />
